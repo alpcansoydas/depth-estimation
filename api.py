6d1ca07d3b9e68fd83da41e2709174a550e5135d
alpcansoydas <alpcan50@gmail.com>
2024-01-03 10:58:23 +0300

    imgbb added to api

--- api.py
+++ api.py
@@ -2,24 +2,24 @@ from fastapi import FastAPI, File, UploadFile
 from predictor import DepthEstimationModel
 import os
 import uuid
+from upload import upload_image_to_imgbb
 
 app = FastAPI()
 depth_estimator = DepthEstimationModel()
-ALLOWED_EXTENSIONS = ['.jpg', '.png', 'jpeg']
+ALLOWED_EXTENSIONS = ['.jpg', '.png', '.jpeg']
 TEMP_FOLDER = "api_images"
-os.makedirs('TEMP_FOLDER', exist_ok=True)
+os.makedirs(TEMP_FOLDER, exist_ok=True)
 
 @app.post('/predict')
 async def predict(file: UploadFile = File(...)):
     try:
-        file_ext = os.path.splittext(file.filename)[1]
+        file_ext = os.path.splitext(file.filename)[1]
         if file_ext not in ALLOWED_EXTENSIONS:
             return {'error': '''Invalid file type! 
                     Uploaded file must be in JPG, JPEG or PNG format.'''}
         
         filename_base = str(uuid.uuid4())
         filename = filename_base + file_ext
-
         destination_path = os.path.join(TEMP_FOLDER, filename)
         output_path = os.path.join(TEMP_FOLDER, "output" + filename_base + ".png")
 
@@ -27,7 +27,8 @@ async def predict(file: UploadFile = File(...)):
             image_data.write(file.file.read())
         
         depth_estimator.calculate_depthmap(destination_path, output_path)
+        response = upload_image_to_imgbb(output_path)
+        return response
 
     except Exception as e:
         return {'error': str(e)}
-
